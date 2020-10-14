d8ff22ca99c5414e266510b255e67893625ca974
sniggy1325 <71495725+sniggy1325@users.noreply.github.com>
2020-10-14 17:33:12 +0530

    Add files via upload

---
+++ project-21/sketch.js
@@ -0,0 +1,64 @@
+var bullet , wall, speed, weight, thickness;
+ function setup() {
+    createCanvas(1600,400);
+    bullet = createSprite (50, 200, 50, 30);
+    bullet.shapeColor="white"
+    speed = random(223,321);
+    weight = random(30,52);
+   bullet.velocityX = speed;
+   thickness = random(22,83);
+     wall = createSprite (1200, 200, thickness, height/2);
+     wall.shapeColor= color(80,80,80);
+     
+    
+    
+    
+ }
+    
+   function draw() {
+      background(0)
+
+      if(hasCollided(bullet, wall)){
+
+         bullet.velocityX=0;
+         var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+          
+      if(damage>10){
+           
+         wall.shapeColor=color(255,0,0);
+      }
+
+
+      if (damage<10){
+
+         wall.shapeColor=color(0,255,0)
+      }
+
+      }
+      drawSprites()
+   }  
+     
+    
+   
+   function hasCollided(lbullet, lwall)
+   {
+      bulletRightEdge=lbullet.x+lbullet.width;
+      wallLeftEdge=lwall.x;
+
+      if(bulletRightEdge>=wallLeftEdge){
+         return true;
+      }
+         return false;
+   }
+
+
+  
+
+
+      
+      
+      
+      
+     
+           
+
